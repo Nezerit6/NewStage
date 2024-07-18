f1bd60be43a2b044a091033c3acf5507f71a1fd4
Nezerit6 <nezerit66@gmail.com>
2024-07-18 16:04:55 +0300

    final pre-alpha 0.1 update

--- src/nstage/content/NewStageUnits.java
+++ src/nstage/content/NewStageUnits.java
@@ -58,18 +58,74 @@ public class NewStageUnits {
 
         frontis = new UnitType("frontis") {{
             constructor = UnitEntity::create;
-            health = 50;
-            hitSize = 11f;
-            speed = 2.4f;
-            rotateSpeed = 5.4f;
-            drag = 0.03f;
-            accel = 0.1f;
             flying = true;
-            targetAir = true;
-            targetGround = false;
-            engineOffset = 5.7f;
-            lowAltitude = true;
-            itemCapacity = 5;
+            drag = 0.05f;
+            speed = 2.1f;
+            rotateSpeed = 9.4f;
+            accel = 0.1f;
+            range = 130f;
+            health = 600;
+            engineOffset = 8.5f;
+            hitSize = 12f;
+            faceTarget = false;
+
+            weapons.add(new Weapon("nstage-w1") {{
+                top = true;
+                mirror = true;
+                y = 5.7f;
+                x = 4f;
+                reload = 30f;
+                recoil = 1f;
+                shootSound = Sounds.missile;
+                velocityRnd = 0.5f;
+                inaccuracy = 15f;
+                rotationLimit = 160f;
+                rotateSpeed = 3.5f;
+                rotate = true;
+
+                bullet = new BasicBulletType(6f, 7) {{
+                    lifetime = 60f;
+                    trailLength = 15;
+                    trailWidth = 1.6f;
+                    trailSinScl = 2f;
+                    trailSinMag = 0.5f;
+                    trailEffect = Fx.none;
+                    trailColor = Color.valueOf("FF6F89FF");
+                    frontColor = Color.white;
+                    backColor = Color.valueOf("D85876FF");
+                    width = 5;
+                    height = 6;
+                }};
+            }});
+
+            weapons.add(new Weapon("nstage-w2") {{
+                top = true;
+                mirror = true;
+                y = -1.6f;
+                x = -4.5f;
+                reload = 30f;
+                recoil = 1f;
+                shootSound = Sounds.missile;
+                velocityRnd = 0.5f;
+                inaccuracy = 15f;
+                rotationLimit = 70f;
+                rotateSpeed = 2f;
+                rotate = true;
+
+                bullet = new BasicBulletType(2.5f, 18) {{
+                    lifetime = 60f;
+                    trailLength = 15;
+                    trailWidth = 1.6f;
+                    trailSinScl = 2f;
+                    trailSinMag = 0.5f;
+                    trailEffect = Fx.none;
+                    trailColor = Color.valueOf("FF6F89FF");
+                    frontColor = Color.white;
+                    backColor = Color.valueOf("D85876FF");
+                    width = 7;
+                    height = 8;
+                }};
+            }});
         }};
 
         lenta = new UnitType("lenta") {{
@@ -97,7 +153,7 @@ public class NewStageUnits {
         UnitFactory airFactory = (UnitFactory) Blocks.airFactory;
         airFactory.plans.add(new UnitFactory.UnitPlan(navicula, 60f * 5, ItemStack.with(Items.silicon, 30, Items.lead, 20)));
         //T2
-        Reconstructor additiveReconstructor = (Reconstructor) Blocks.additiveReconstructor;
-        additiveReconstructor.upgrades.add(new UnitType[]{NewStageUnits.navicula, frontis});
+        /*Reconstructor additiveReconstructor = (Reconstructor) Blocks.additiveReconstructor;
+        additiveReconstructor.upgrades.add(new UnitType[]{NewStageUnits.navicula, frontis});*/
     }
 }
